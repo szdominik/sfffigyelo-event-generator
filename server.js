b0b6daf2157563a485d41310fa4d804bfc3c0254
szdominik <dominikblasir@gmail.com>
2018-02-23 21:07:14 +0100

    feat(server): add express server

---
+++ server.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(3000, () => {
+    console.log('App is listening on port 3000...');
+});
