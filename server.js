23fe31485cf7d00eeeea9fa6ed60b4c9f4673844
szdominik <dominikblasir@gmail.com>
2018-02-23 21:14:11 +0100

    fix(server): specify port

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.status(200).sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('App is listening on port 3000...');
 });
