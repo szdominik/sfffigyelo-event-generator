e3ba4e1fc81a649dfeb5cd97fdf97d59ec1ad8c4
szdominik <dominikblasir@gmail.com>
2018-02-23 21:17:32 +0100

    fix(server): serving static files

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (req,res) => {
     res.status(200).sendFile(path.join(__dirname + '/index.html'));
 });
