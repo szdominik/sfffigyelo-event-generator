3609075d15a114d4bdc10b9fc4c081b591464553
szdominik <dominikblasir@gmail.com>
2018-02-23 21:20:27 +0100

    fix(server): move files to serving static files

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (req,res) => {
-    res.status(200).sendFile(path.join(__dirname + '/index.html'));
+    res.status(200).sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 app.listen(process.env.PORT || 3000, () => {
